f1c3d0ac01bcf71a22dfa8d75185fb642072b6f9
inga-divra <ingady@gmail.com>
2022-12-02 12:18:34 +0200

    finished project

--- app.js
+++ app.js
@@ -31,3 +31,53 @@ sidebar.innerHTML = sublinks.map((item) => {
     </div>
     </article>`
 }).join("");
+
+linkBtns.forEach((btn) => {
+    console.log(btn);
+    btn.addEventListener('mouseover', function (e) {
+        const text = e.currentTarget.textContent;
+        const tempBtn = e.currentTarget.getBoundingClientRect()
+        const center = (tempBtn.left + tempBtn.right) / 2;
+        const bottom = tempBtn.bottom - 3;
+
+        const tempPage = sublinks.find(({ page }) => page === text);
+        if (tempPage) {
+            const { page, links } = tempPage;
+            submenu.classList.add('show');
+            submenu.style.left = `${center}px`;
+            submenu.style.top = `${bottom}px`;
+
+            /* Optional columns layout */
+            let columns = 'col-2';
+            if (links.length === 3) {
+                columns = 'col-3';
+            }
+            if (links.length > 3) {
+                columns = 'col-4';
+            }
+
+            submenu.innerHTML = `
+            <section>
+            <h4>${page}</h4>
+            <div class="submenu-center ${columns}">
+            ${links.map((link) => {
+                return `<a href="${link.url}">
+                <i class="${link.icon}"></i> ${link.label}
+                </a>`
+            }).join('')}
+            </div>
+            </section>
+            `
+        }
+    });
+});
+
+hero.addEventListener('mouseover', function () {
+    submenu.classList.remove('show');
+});
+
+nav.addEventListener('mouseover', function (e) {
+    if (!e.target.classList.contains('link-btn')) {
+        submenu.classList.remove('show')
+    }
+})
